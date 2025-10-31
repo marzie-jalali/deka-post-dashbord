efd838e66269c3276bae183290fc5f423f7a60f2
Nura Jalali <jalalinura@gmail.com>
2025-10-31 21:46:56 +0330

    fix: fixed sidebar style

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,12 +5,12 @@ import { useTranslation } from "react-i18next";
 const Sidebar = () => {
   const { t } = useTranslation();
   return (
-    <aside className="w-16 md:w-32 lg:w-48 border border-border bg-card">
-      <nav className="flex flex-col gap-2 p-2">
+    <aside className="w-24 md:w-32 lg:w-48 border border-border bg-card">
+      <nav className="flex flex-col gap-1 md:gap-2 lg:gap-3 p-1 md:p-2 lg:p-3">
         <NavLink
           to={pathes.dashboard}
           className={({ isActive }) =>
-            `flex items-center gap-2 p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors ${
+            `flex items-center gap-1 md:gap-2 lg:gap-3 p-2 md:p-3 lg:p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors text-xs md:text-sm lg:text-base ${
               isActive ? "bg-accent text-accent-foreground" : ""
             }`
           }
@@ -20,7 +20,7 @@ const Sidebar = () => {
         <NavLink
           to={pathes.todos}
           className={({ isActive }) =>
-            `flex items-center gap-2 p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors ${
+            `flex items-center gap-1 md:gap-2 lg:gap-3 p-2 md:p-3 lg:p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors text-xs md:text-sm lg:text-base ${
               isActive ? "bg-accent text-accent-foreground" : ""
             }`
           }
@@ -30,7 +30,7 @@ const Sidebar = () => {
         <NavLink
           to={pathes.weather}
           className={({ isActive }) =>
-            `flex items-center gap-2 p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors ${
+            `flex items-center gap-1 md:gap-2 lg:gap-3 p-2 md:p-3 lg:p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors text-xs md:text-sm lg:text-base ${
               isActive ? "bg-accent text-accent-foreground" : ""
             }`
           }
@@ -40,7 +40,7 @@ const Sidebar = () => {
         <NavLink
           to={pathes.profile}
           className={({ isActive }) =>
-            `flex items-center gap-2 p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors ${
+            `flex items-center gap-1 md:gap-2 lg:gap-3 p-2 md:p-3 lg:p-4 rounded-md hover:bg-accent text-card-foreground hover:text-accent-foreground transition-colors text-xs md:text-sm lg:text-base ${
               isActive ? "bg-accent text-accent-foreground" : ""
             }`
           }
